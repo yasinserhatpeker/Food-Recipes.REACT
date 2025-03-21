81165a30eb9634509a00fd0996bdf67bd0f517b9
Yasin <yasinpeker43@gmail.com>
2025-03-21 10:26:01 +0300

    ul and li elements are added to Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,11 @@ const Navbar = () => {
     <nav className='navbar navbar-expand-lg navbar-dark navbar-secondary'>
          <div className="container">
            <Link className='navbar-brand'to="/">Yemek Tarifleri</Link>
+           <ul className='navbar-nav'>
+            <li className='nav-item'>
+             <NavLink to='/' className="nav-link">Home</NavLink>
+            </li>
+           </ul>
          </div>
     </nav>
   )
